e4572157a254f2fa82076703b28b6f3ef9ac8030
bbuermann-it <buermann.bjoern@gmail.com>
2023-11-25 20:17:03 +0100

    ViewController  - changed include layout routes - added post login route notFound.view.php - added temporarily custom 404 view loginIndex.view.php - changed style to flexbox LoginController.php - renamed method and added post login method index.php - refactoring and logic improvement autoloader.php - improved logic and fixed bug

--- src/controller/ViewController.php
+++ src/controller/ViewController.php
@@ -1,12 +1,11 @@
 <?php
-
 namespace src\controller;
+
 
 class ViewController
 {
-
     public function __construct($viewName)
     {
-        include_once("../src/views/{$viewName}.view.php");
+        include_once("..\\src\\views\\{$viewName}.view.php");
     }
 }
