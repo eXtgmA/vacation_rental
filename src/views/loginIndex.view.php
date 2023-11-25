8209b8af5e32251c7a518324d9781f9f9bfdb40d
bbuermann-it <buermann.bjoern@gmail.com>
2023-11-25 01:36:29 +0100

    First draft of loginpage with partial form and input fields

---
+++ src/views/loginIndex.view.php
@@ -0,0 +1,31 @@
+<?
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1>Login Page</h1>
+<h3>Bitte einloggen</h3>
+
+<form action="" method="post">
+    <label for="username">Username</label>
+    <input type="text" name="username">
+    <br>
+    <label for="password">Password</label>
+    <input type="password" name="password">
+    <br>
+    <button type="submit">Login</button>
+</form>
+<br>
+<h3>Noch keinen Account ?
+    <form action="">
+        <button type="submit">Account Anlegen</button>
+    </form></h3>
+</body>
+</html>
