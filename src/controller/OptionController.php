0c12c6f8a6b87c205de9d1a050c24e5fba98037e
finkeRe <gith-rf@dfr-mail.de>
2023-12-03 03:04:44 +0100

    Implement postCreate() for Options

--- src/controller/OptionController.php
+++ src/controller/OptionController.php
@@ -2,6 +2,7 @@
 namespace src\controller;
 
 use src\models\Option;
+use src\models\Image;
 
 class OptionController extends BaseController
 {
@@ -24,11 +25,23 @@ class OptionController extends BaseController
 
     public function postCreate() : void
     {
-        $param = [];
-        foreach ($_REQUEST as $key => $value) {
-            $param[$key] = $value;
-        }
+        // feed image parameters into image creation
+        $image_param["id"] = $_REQUEST["image_id"];
+        $image_param["uuid"] = $_REQUEST["uuid"];
+        $image_param["house_id"] = $_REQUEST["house_id"];
+        $image_param["typetable_id"] = $_REQUEST["typetable_id"];
+//        $image = new Image();
+//        $image->addImage($image_param);
+
+        // option parameters
+        $option_param["id"] = $_REQUEST["option_id"];
+        $option_param["name"] = $_REQUEST["name"];
+        $option_param["description"] = $_REQUEST["description"];
+        $option_param["price"] = $_REQUEST["price"];
+        $option_param["is_disabled"] = $_REQUEST["is_disabled"];
+        $option_param["house_id"] = $_REQUEST["house_id"];
+//        $option_param["image_id"] = $image->getId();
         $option = new Option();
-        $option->addOption($param);
+        $option->addOption($option_param);
     }
 }
