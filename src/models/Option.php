ea2e0392c56ae377ebe5a56424b8f8d6409a681e
finkeRe <gith-rf@dfr-mail.de>
2023-12-03 01:26:17 +0100

    Add model Option

---
+++ src/models/Option.php
@@ -0,0 +1,110 @@
+<?php
+namespace src\models;
+
+use Exception;
+use mysqli_result;
+
+class Option extends BaseModel
+{
+    private int $id;
+    private string $name;
+    private string $description;
+    private float $price;
+    private bool $is_disabled;
+    private int $house_id;
+    private int $image_id;
+
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Retrieves an Option from the database specified by its index
+     *
+     * @param int $option_id
+     * @return Option|null
+     */
+    public function getOption(int $option_id): Option|null
+    {
+        try {
+            $query = "select * from options where id = '{$option_id}' limit 1";
+            $sql = $this->connection->query($query);
+            if ($sql instanceof mysqli_result) {
+                $result = $sql->fetch_object('\src\models\Option');
+                // if all ok return Option
+                if (isset($result) && $result instanceof Option) {
+                    return $result;
+                // if no Option could be fetched -> throw exception
+                } else {
+                    error_log('Option with id "' . $option_id . '" could not be fetched.');
+                    throw new Exception('Option konnte nicht geladen werden.');
+                }
+            }
+        } catch (Exception $exception) {
+            $_SESSION['message'] = $exception->getMessage();
+        }
+        return null;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): void
+    {
+        $this->price = ($price >= 0) ? $price : 0;
+    }
+
+    public function isIsDisabled(): bool
+    {
+        return $this->is_disabled;
+    }
+
+    public function setIsDisabled(bool $is_disabled): void
+    {
+        $this->is_disabled = $is_disabled;
+    }
+
+    public function getHouseId(): int
+    {
+        return $this->house_id;
+    }
+
+    public function getImageId(): int
+    {
+        return $this->image_id;
+    }
+}
