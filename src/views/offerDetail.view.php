c80c0d0fb2298658379c0beb71f4b48535bacb41
finkeRe <gith-rf@dfr-mail.de>
2023-12-08 13:00:08 +0100

    Expand offer detail view and link to options

--- src/views/offerDetail.view.php
+++ src/views/offerDetail.view.php
@@ -6,7 +6,66 @@ $house = isset($param) ? $param : null;
 include_once($header);
 ?>
 <!--Hier den HTML Inhalt einfuegen-->
-<h1>Details für Haus <?php echo $house->getName();?> <a href="/offer"><p class="fa fa-chevron-left"></p> zurück</a></h1>
+<div>
+    <a href="/offer"><p class="fa fa-chevron-left"></p> zurück</a>
+</div>
+<h1>Details für Haus <?php echo $house->getName();?></h1>
+<table>
+    <tr>
+        <td>Name der Anlage</td>
+        <td><p><?php echo $house->getName();?></p></td>
+    </tr>
+    <tr>
+        <td>Beschreibung</td>
+        <td><p><?php echo $house->getDescription();?></p></td>
+    </tr>
+    <tr>
+        <td>Quadratmeter</td>
+        <td><p><?php echo $house->getSquareMeter();?></p></td>
+    </tr>
+    <tr>
+        <td>Anzahl mögliche Personen</td>
+        <td><p><?php echo $house->getMaxPerson();?></p></td>
+
+    <tr>
+        <td>Anzahl Räume</td>
+        <td><p><?php echo $house->getRoomCount();?></p></td>
+    </tr>
+    <tr>
+        <td>Preis pro Nacht</td>
+        <td><p><?php echo $house->getPrice();?></p></td>
+    </tr>
+    <tr>
+        <td>Postleitzahl</td>
+        <td><p><?php echo $house->getPostalCode();?></p></td>
+    </tr>
+    <tr>
+        <td>Ort</td>
+        <td><p><?php echo $house->getCity();?></p></td>
+    </tr>
+    <tr>
+        <td>Straße</td>
+        <td><p><?php echo $house->getStreet();?></p></td>
+    </tr>
+    <tr>
+        <td>Hausnummer</td>
+        <td><p><?php echo $house->getHouseNumber();?></p></td>
+    </tr>
+    <tr>
+        <td>Foto</td>
+        <td>
+            <img src="<?php echo "/images/".$house->getFrontimage();?>" style="width: 100px;height: 100px" alt="alt">
+        </td>
+    </tr>
+    <tr>
+        <td>Status</td>
+        <td><p><?php echo $house->getIsDisabled();?></p></td>
+    </tr>
+</table>
+<div>
+    <a href="<?php echo "/option/show/".$house->getId();?>"><h1>Show all Options</h1></a>
+    <a href="<?php echo "/option/create/".$house->getId();?>"><h1>Create Option</h1></a>
+</div>
 <!--Ende HTML Inhalt-->
 <?php
 $footer=__DIR__."/partials/footer.view.php";
