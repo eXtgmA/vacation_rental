3a23276b623b93dbde7203a7f2031e45562f631b
Marvin Künkler <kuenklerm@telekom.de>
2023-12-11 13:35:23 +0100

    112 - create a calendar widget

---
+++ public/scripts/calendar-widget.js
@@ -0,0 +1,81 @@
+console.log("calendar-widget.js loaded");   // todo: remove
+
+//   load default styles
+const link = document.createElement('link');
+link.rel = 'stylesheet';
+link.href = '/styles/calendar-widget.css';
+document.head.appendChild(link);
+
+// Calendar Widget
+let currentMonth = new Date().getMonth();
+let currentYear = new Date().getFullYear();
+let weekdays = ['Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa', 'So']; // Start with Monday
+let months = ['Januar', 'Februar', 'März', 'April', "Mai", "Juni", "Juli", "August", "September", "Oktober",
+    "November", "Dezember"];
+let bookedDays = ['2023-09-03', '2023-09-07', '2023-09-14', '2023-09-21', '2023-11-10', '2023-11-11', '2023-11-12',
+    '2023-11-13', '2023-11-14', '2023-12-20', '2023-12-21', '2023-12-22', '2023-12-23', '2023-12-24', '2023-12-25',
+    '2023-12-26', '2023-12-27'];
+
+function drawCalendar(elementId, month, year, bookedDays = []) {
+    // remove spaces from elementId
+    elementId = elementId.replace(/\s/g, '');
+
+    console.log(elementId);
+    let daysInMonth = new Date(year, month + 1, 0).getDate();
+    let firstDayOfMonth = new Date(year, month, 1).getDay();
+    let calendar = document.getElementById(elementId);
+    calendar.innerHTML = `<div class="month" style="grid-column: span 7;">
+                                 <button id="${elementId}-prev" class="btn-prev">&#8592;</button>
+                                 <span style="font-weight: bolder"> ${months[month]} ${year}</span>
+                                 <button id="${elementId}-next" class="btn-next">&#8594;</button>
+                               </div>`;
+
+    // Add the weekdays to the calendar
+    for (let i = 0; i < weekdays.length; i++) {
+        calendar.innerHTML += `<div class="weekday">${weekdays[i]}</div>`;
+    }
+
+    // Adjust the first day of the month for the new weekdays array
+    firstDayOfMonth = firstDayOfMonth === 0 ? 6 : firstDayOfMonth - 1;
+
+    // Add empty days before the first day of the month
+    for (let i = 0; i < firstDayOfMonth; i++) {
+        calendar.innerHTML += `<div class="day"></div>`;
+    }
+
+    // Add the days of the month
+    for (let i = 1; i <= daysInMonth; i++) {
+        let currentDate = `${year}-${('0' + (month + 1)).slice(-2)}-${('0' + i).slice(-2)}`;
+        // Check if the current day is a booked day
+        if (bookedDays.includes(currentDate)) {
+            // Add booked class to the day
+            calendar.innerHTML += `<div class="day booked">${i}</div>`;
+        } else {
+            calendar.innerHTML += `<div class="day">${i}</div>`;
+        }
+    }
+
+    // Add click events to the prev and next buttons
+    document.getElementById(elementId + '-prev').onclick = function () {
+        if (month === 0) {
+            month = 11;
+            year--;
+        } else {
+            month--;
+        }
+        drawCalendar(elementId, month, year, bookedDays);
+    };
+
+    // Add click events to the prev and next buttons
+    document.getElementById(elementId + '-next').onclick = function () {
+        if (month === 11) {
+            month = 0;
+            year++;
+        } else {
+            month++;
+        }
+        drawCalendar(elementId, month, year, bookedDays);
+    };
+}
+
+
--- src/views/offerIndex.view.php
+++ src/views/offerIndex.view.php
@@ -4,7 +4,38 @@ $header = __DIR__ . "/partials/header.view.php";
 $title = "";
 $page = 'offerindex';
 include_once($header);
+
+
+
 ?>
+<link rel="stylesheet" href="/styles/offer.css"/>
+<script src="/scripts/calendar-widget.js"></script>
+
+<?php
+if (isset($param)) {
+    foreach ($param as $house) {
+        ?>
+        <div class="horizontal-card">
+            <div class="card-image">
+                <img src="/images/<?php print $house->getFrontImage() ?>" alt="alt">
+            </div>
+            <div class="card-details">
+                <h2><?php print $house->getName() ?></h2>
+                <button class="btn-secondary">Bearbeiten</button>
+                <button class="btn-warning">Deaktivieren</button>
+            </div>
+<!--            <div id="calendar"></div>-->
+            <div id="calendar-<?php print $house->getId() ?>" class="calendar">
+            </div>
+            <script>
+                drawCalendar("calendar  -<?php echo $house->getId() ?>", currentMonth, currentYear, bookedDays);
+            </script>
+        </div>
+        <?php
+    }
+}
+?>
+
 <!--Hier den HTML Inhalt einfuegen-->
 <a href="/offer/create">Neues Haus anlegen</a>
 
@@ -13,50 +44,54 @@ include_once($header);
 
     <?php
     if (isset($param)) {
-        echo "<table>"
-        ?>
-        <tr>
-            <th>Name</th>
-            <th>Quadratmeter</th>
-            <th>Anzahl Zimmer</th>
-            <th>Max Personen</th>
-            <th>Preis</th>
-            <th>Postleitzahl</th>
-            <th>Stadt</th>
-            <th>Straße</th>
-            <th>Hausnummer</th>
-            <th>status</th>
-            <th>Detail</th>
-            <th>Front</th>
-        </tr>
-        <?php
-        /** @var \src\models\House[] $param */
-        foreach ($param as $house) {
-            echo "<tr>";
-            echo "<td>" . $house->getName() . "</td>";
-            echo "<td>" . $house->getSquareMeter() . "</td>";
-            echo "<td>" . $house->getRoomCount() . "</td>";
-            echo "<td>" . $house->getMaxPerson() . "</td>";
-            echo "<td>" . $house->getPrice() . "</td>";
-            echo "<td>" . $house->getPostalCode() . "</td>";
-            echo "<td>" . $house->getCity() . "</td>";
-            echo "<td>" . $house->getStreet() . "</td>";
-            echo "<td>" . $house->getHouseNumber() . "</td>";
-            echo "<td>" . $house->getIsDisabled() . "</td>";
-            echo "<td><a href='/offer/show/" . $house->getId() . "'><i class='fa fa-house'></i></a></td>";
-            ?>
-        </td>
-        <td><img src="/images/<?php print $house->getFrontImage() ?>" style="width: 30px;height: 30px" alt="alt"></td>
-            <td>
-            <form action="/offer/togglestatus/<?php echo $house->getId(); ?>" method="post">
-                <button type="submit"><i class="fa <?php $house->getIsDisabled()==1 ? print('fa-eye-slash') : print('fa-eye')?>"></i></button>
-            </form>
+    echo "<table>"
+    ?>
+    <tr>
+        <th>Name</th>
+        <th>Quadratmeter</th>
+        <th>Anzahl Zimmer</th>
+        <th>Max Personen</th>
+        <th>Preis</th>
+        <th>Postleitzahl</th>
+        <th>Stadt</th>
+        <th>Straße</th>
+        <th>Hausnummer</th>
+        <th>status</th>
+        <th>Detail</th>
+        <th>Front</th>
+    </tr>
+    <?php
+    /** @var \src\models\House[] $param */
+    foreach ($param
 
-            <?php
+    as $house) {
+    echo "<tr>";
+    echo "<td>" . $house->getName() . "</td>";
+    echo "<td>" . $house->getSquareMeter() . "</td>";
+    echo "<td>" . $house->getRoomCount() . "</td>";
+    echo "<td>" . $house->getMaxPerson() . "</td>";
+    echo "<td>" . $house->getPrice() . "</td>";
+    echo "<td>" . $house->getPostalCode() . "</td>";
+    echo "<td>" . $house->getCity() . "</td>";
+    echo "<td>" . $house->getStreet() . "</td>";
+    echo "<td>" . $house->getHouseNumber() . "</td>";
+    echo "<td>" . $house->getIsDisabled() . "</td>";
+    echo "<td><a href='/offer/show/" . $house->getId() . "'><i class='fa fa-house'></i></a></td>";
+    ?>
+    </td>
+    <td><img src="/images/<?php print $house->getFrontImage() ?>" style="width: 30px;height: 30px" alt="alt"></td>
+    <td>
+        <form action="/offer/togglestatus/<?php echo $house->getId(); ?>" method="post">
+            <button type="submit"><i
+                        class="fa <?php $house->getIsDisabled() == 1 ? print('fa-eye-slash') : print('fa-eye') ?>"></i>
+            </button>
+        </form>
+
+        <?php
         }
         echo "</table>";
-    }
-    ?>
+        }
+        ?>
 
 
 </div>
@@ -65,3 +100,4 @@ include_once($header);
 $footer = __DIR__ . "/partials/footer.view.php";
 include_once($footer)
 ?>
+
