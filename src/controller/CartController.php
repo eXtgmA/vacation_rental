36e32704da12f569620c35c02153d5f77373e366
finkeRe <gith-rf@dfr-mail.de>
2023-12-14 21:05:09 +0100

    Add displaying of houses to cart

--- src/controller/CartController.php
+++ src/controller/CartController.php
@@ -18,10 +18,29 @@ class CartController extends BaseController
             /** @var Booking|null $booking */
             $booking = $this->find('\src\models\Booking', 'is_confirmed', 0, 1);
             $param["booking"] = $booking;
+
             if ($booking != null) {
                 // get all bookingpositions related to this booking
                 $param["bookingpositions"] = $booking->getAllBookingpositions();
-                // todo : get all houses related to all bookingpositions
+                // if no positions found show empty cart
+                if ($param["bookingpositions"] == false) {
+                    new ViewController('cart');
+                    die();
+                }
+
+                // get all houses related to all bookingpositions
+                $houseIds = [];
+                foreach ($param["bookingpositions"] as $bp) {
+                    // fetch house if not already loaded
+                    if (!in_array($bp->getHouseId(), $houseIds)) {
+                        $param["houses"][$bp->getHouseId()] = $this->find('\src\models\House', 'id', $bp->getHouseId(), 1);
+                        $houseIds[] = $bp->getHouseId();
+                    }
+                }
+                // check that at least one house has been fetched
+                if (empty($param["houses"])) {
+                    throw new \Exception("No house loaded");
+                }
             }
         } catch (\Exception $e) {
             $_SESSION['message'] = "Ein unerwarteter Fehler ist aufgetreten";
--- src/views/cart.view.php
+++ src/views/cart.view.php
@@ -5,6 +5,7 @@ $header=__DIR__."/partials/header.view.php";
 $title = "Warenkorb";
 $booking = $param["booking"] ?? null;
 $bpos = $param["bookingpositions"] ?? null;
+$houses = $param["houses"] ?? null;
 include_once($header);
 ?>
 <!--Hier den HTML Inhalt einfuegen-->
@@ -13,11 +14,19 @@ include_once($header);
     echo($message ?? "<h1>$message</h1>");
     ?>
 <div>
-    <?php if (isset($booking) && isset($bpos)) {
+    <?php if (isset($booking, $bpos, $houses)) {
         echo "<table>";
         /** @var \src\models\Bookingposition $p */
         foreach ($bpos as $key => $p) {
+            /** @var \src\models\House $house */
+            $house = $houses[$p->getHouseId()];
             ?>
+            <tr>
+                <td><h3><?php echo $house->getName() ?></h3></td>
+            </tr>
+            <td>
+                <img src="<?php echo "/images/".$house->getFrontimage();?>" style="width: 100px;height: 100px" alt="[alt]">
+            </td>
             <tr>
                 <td>Von</td>
                 <td><?php echo $p->getDateStart() ?></td>
@@ -27,12 +36,28 @@ include_once($header);
                 <td><?php echo $p->getDateEnd() ?></td>
             </tr>
             <tr>
-                <td>Price Details</td>
-                <td><?php print_r($p->getPriceDetailList()); ?></td> <!-- todo : expand list in detail -->
+                <td>Preis</td>
+                <td><?php echo $house->getPrice()." Euro/Nacht" ?></td>
             </tr>
             <tr>
-                <td>House id</td>
-                <td><?php echo $p->getHouseId() ?></td>
+                <td>Ort</td>
+                <td><?php echo $house->getPostalCode(). ", " .$house->getCity() ?></td>
+            </tr>
+            <tr>
+                <td>Strasse</td>
+                <td><?php echo $house->getStreet() ." ". $house->getHouseNumber() ?></td>
+            </tr>
+            <tr>
+                <td>Personen</td>
+                <td><?php echo "max.".$house->getMaxPerson() ?></td>
+            </tr>
+            <tr>
+                <td>Fläche</td>
+                <td><?php echo $house->getSquareMeter() ?></td>
+            </tr>
+            <tr>
+                <td>Räume</td>
+                <td><?php echo $house->getRoomCount() ?></td>
             </tr>
             <?php
         }
