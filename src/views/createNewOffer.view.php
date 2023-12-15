baaf02325d7b4b3afecf7ac9ab216c6586522fcf
eXtgmA <marvinkuenkler@gmail.com>
2023-12-15 01:26:24 +0100

    116 - make offer create page responsive and mobile able

--- src/views/createNewOffer.view.php
+++ src/views/createNewOffer.view.php
@@ -124,7 +124,7 @@ include_once($header);
         <div id="feature-grid">
             <div class="feature-select-list">
                 <div class="feature-topic">
-                    <h3>Outdoor</h3>
+                    <h3 class="sub-headline">Outdoor</h3>
                 </div>
                 <label class="feature-select">
                     <input type="checkbox">
@@ -157,7 +157,7 @@ include_once($header);
             </div>
             <div class="feature-select-list">
                 <div class="feature-topic">
-                    <h3>Wellness</h3>
+                    <h3 class="sub-headline">Wellness</h3>
                 </div>
                 <label class="feature-select">
                     <input type="checkbox">
@@ -178,7 +178,7 @@ include_once($header);
             </div>
             <div class="feature-select-list">
                 <div class="feature-topic">
-                    <h3>Bad</h3>
+                    <h3 class="sub-headline">Bad</h3>
                 </div>
                 <label class="feature-select">
                     <input type="checkbox">
@@ -191,7 +191,7 @@ include_once($header);
             </div>
             <div class="feature-select-list">
                 <div class="feature-topic">
-                    <h3>Multimedia</h3>
+                    <h3 class="sub-headline">Multimedia</h3>
                 </div>
                 <label class="feature-select">
                     <input type="checkbox">
@@ -212,7 +212,7 @@ include_once($header);
             </div>
             <div class="feature-select-list">
                 <div class="feature-topic">
-                    <h3>Küche</h3>
+                    <h3 class="sub-headline">Küche</h3>
                 </div>
                 <label class="feature-select">
                     <input type="checkbox">
@@ -241,7 +241,7 @@ include_once($header);
             </div>
             <div class="feature-select-list">
                 <div class="feature-topic">
-                    <h3>Sonstiges</h3>
+                    <h3 class="sub-headline">Sonstiges</h3>
                 </div>
                 <label class="feature-select">
                     <input type="checkbox">
@@ -297,67 +297,6 @@ include_once($header);
 </form>
 
 
-<!--Hier den HTML Inhalt einfuegen-->
-
-
-<div style="justify-content: center">
-    <div>
-        <h3><?php isset($message) ? print ($message) : print '' ?></h3>
-        <form action="/offer/create" method="post" enctype="multipart/form-data">
-
-            <table>
-                <tr>
-                    <td>Name der Anlage</td>
-                    <td><input type="Text" name="name" value="<?php pold('name') ?>"></td>
-                </tr>
-                <tr>
-                    <td>Beschreibung</td>
-                    <td><textarea name="description" id="" cols="30" rows="10"><?php pold('description') ?></textarea>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Quadratmeter</td>
-                    <td><input type="number" name="square_meter" min="1" value="<?php pold('square_meter') ?>"></td>
-                </tr>
-                <tr>
-                    <td>Anzahl mögliche Personen</td>
-                    <td><input type="number" name="max_person" min="1" value="<?php pold('max_person') ?>"></td>
-
-                <tr>
-                    <td>Anzahl Räume</td>
-                    <td><input type="number" name="room_count" min="1" value="<?php pold('room_count') ?>"></td>
-                </tr>
-                <tr>
-                    <td>Preis pro Nacht</td>
-                    <td><input type="number" name="price" min="1" value="<?php pold('price') ?>"></td>
-                </tr>
-                <tr>
-                    <td>Postleitzahl</td>
-                    <td><input type="number" name="postal_code" maxlength="5" value="<?php pold('postal_code') ?>"></td>
-                </tr>
-                <tr>
-                    <td>Ort</td>
-                    <td><input type="text" name="city" value="<?php pold('city') ?>"></td>
-                </tr>
-                <tr>
-                    <td>Straße</td>
-                    <td><input type="text" name="street" value="<?php pold('street') ?>"></td>
-                </tr>
-                <tr>
-                    <td>Hausnummer</td>
-                    <td><input type="number" name="house_number" min="1" value="<?php pold('house_number') ?>"></td>
-                </tr>
-                <tr>
-                    <td><label for="frontimage">Frontbild <i class="fa fa-camera"></i></label></td>
-                    <td><input type="file" name="frontimage" hidden="hidden" id="frontimage"></td>
-                </tr>
-            </table>
-            <button type="submit">Haus einstellen</button>
-        </form>
-
-    </div>
-</div>
-<!--Ende HTML Inhalt-->
 <?php
 $footer = __DIR__ . "/partials/footer.view.php";
 include_once($footer)
