2c71f19ad62864003b5bbeaf40a6341ad814a662
eXtgmA <marvinkuenkler@gmail.com>
2023-12-17 01:03:13 +0100

    131 - add base grid layout for search page

--- src/views/search.view.php
+++ src/views/search.view.php
@@ -10,7 +10,73 @@ if (isset($param)) {
 }
 include_once($header);
 ?>
-    <link rel="stylesheet" href="/styles/dashboard.css"/>
+    <link rel="stylesheet" href="/styles/search.css"/>
+    <div id="layout-grid">
+        <div id="search-grid">
+            <form action="/offer/find">
+                <div id="destination" class="search-input" style="display: inline-block;text-align: left">
+                    <label id="destination-label" style="display: block" for="destination-input-field">Reiseziel</label>
+                    <input id="destination-input-field" class="input-field" name="destination" type="text">
+                </div>
+                <div id="from-date" class="search-input" style="display: inline-block">
+                    <label id="from-date-label" for="from-date-input-field"
+                           style="display: block;text-align: left">Anreise</label>
+                    <input id="from-date-input-field" class="input-field" name="dateStart" type="date">
+                </div>
+                <div id="to-date" class="search-input" style="display: inline-block">
+                    <label id="to-date-label" for="to-date-label-input-field" style="display: block;text-align: left">Abreise</label>
+                    <input id="to-date-label-input-field" class="input-field" name="dateEnd" type="date">
+                </div>
+                <div id="person-amount" class="search-input" style="display: inline-block">
+                    <label id="person-amount-label" for="person-amount-input-field" style="display: block;text-align: left">Personen</label>
+                    <input id="person-amount-input-field" class="input-field" name="persons" type="number">
+                </div>
+                <div style="margin: 10px">
+                    <button class="btn-secondary">Ferienhaus suchen</button>
+                </div>
+            </form>
+        </div>
+        <div id="filter-grid">
+            <h2>Filter</h2>
+            <div id="tags" class="card">
+                <h4>Tags</h4>
+            </div>
+            <div id="feature-bath" class="card">
+                <h4>Badezimmer</h4>
+            </div>
+            <div id="feature-kitchen" class="card">
+                <h4>Küche</h4>
+            </div>
+            <div id="feature-multimedia" class="card">
+                <h4>Multimedia</h4>
+            </div>
+            <div id="feature-outdoor" class="card">
+                <h4>Outdoor</h4>
+            </div>
+            <div id="feature-other" class="card">
+                <h4>Sonstiges</h4>
+            </div>
+            <div id="feature-wellness" class="card">
+                <h4>Wellness</h4>
+            </div>
+        </div>
+        <div id="results">
+            <h2>Ergebnisse</h2>
+            <?php
+            foreach ($houses as $house) {
+                ?>
+                <div class="card">
+                    <?php print $house->getName() ?>
+                </div>
+                <?php
+            }
+            ?>
+        </div>
+    </div>
+
+
+
+
     <div id="dashboard-grid">
         <div id="title">
             <h1>Finde dein Traumhaus für den perfekten Urlaub</h1>
