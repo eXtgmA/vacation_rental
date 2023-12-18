b385ac4e712e133ddec8cbfde58b3fdd42683684
eXtgmA <marvinkuenkler@gmail.com>
2023-12-18 12:52:56 +0100

    activate search menu point for search result page

--- src/views/partials/navbar.view.php
+++ src/views/partials/navbar.view.php
@@ -1,5 +1,5 @@
 <?php
-$site = explode('/', $_SERVER['REQUEST_URI'])[1];
+$site = substr(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), 1);
 ?>
 <link rel="stylesheet" href="/styles/navbar.css"/>
 <div class="navparent">
@@ -7,10 +7,10 @@ $site = explode('/', $_SERVER['REQUEST_URI'])[1];
         <img src="/assets/logo.png" alt="logo">
     </div>
     <div class="navlinks">
-        <a class="<?php echo $site=='dashboard'? 'active':'';?>" href="/dashboard" title="Suchen"><i class="fa fa-magnifying-glass"></i></a>
-        <a class="<?php echo $site=='cart'? 'active':'';?>" href="/cart" title="Warenkorb"><i class="fa fa-cart-shopping"></i></a>
-        <a class="<?php echo $site=='profile'? 'active':'';?>" href="" title="Benutzer verwalten"><i class="fa fa-user"></i></a>
-        <a class="<?php echo $site=='offer'? 'active':'';?>" href="/offer" title="Haus vermieten"><i class="fa fa-hand-holding-dollar"></i></a>
+        <a class="<?php echo in_array($site, ['dashboard', 'offer/find']) ? 'active' : '' ?>" href="/dashboard" title="Suchen"><i class="fa fa-magnifying-glass"></i></a>
+        <a class="<?php echo $site == 'cart' ? 'active' : ''; ?>" href="/cart" title="Warenkorb"><i class="fa fa-cart-shopping"></i></a>
+        <a class="<?php echo $site == 'profile' ? 'active' : ''; ?>" href="" title="Benutzer verwalten"><i class="fa fa-user"></i></a>
+        <a class="<?php echo $site == 'offer' && $site != 'offer/find' ? 'active' : ''; ?>" href="/offer" title="Haus vermieten"><i class="fa fa-hand-holding-dollar"></i></a>
         <form action="/logout" method="post">
             <button type="submit"><i class="fa fa-right-from-bracket"></i></button>
         </form>
@@ -18,10 +18,10 @@ $site = explode('/', $_SERVER['REQUEST_URI'])[1];
     <div class="sandwich-menu">
         <i class="fa fa-bars"></i>
         <div class="sandwich-navlinks">
-            <a class="<?php echo $site=='dashboard'? 'active':'';?>" href="/dashboard" title="Suchen"><i class="fa fa-magnifying-glass"></i> Suchen</a>
-            <a class="<?php echo $site=='cart'? 'active':'';?>" href="/cart" title="Warenkorb"><i class="fa fa-cart-shopping"></i> Warenkorb</a>
-            <a class="<?php echo $site=='profile'? 'active':'';?>" href="" title="Benutzer verwalten"><i class="fa fa-user"></i> Benutzer verwalten</a>
-            <a class="<?php echo $site=='offer'? 'active':'';?>" href="/offer" title="Haus vermieten"><i class="fa fa-hand-holding-dollar"></i> Haus vermieten</a>
+            <a class="<?php echo in_array($site, ['dashboard', 'offer/find']) ? 'active' : ''; ?>" href="/dashboard" title="Suchen"><i class="fa fa-magnifying-glass"></i> Suchen</a>
+            <a class="<?php echo $site == 'cart' ? 'active' : ''; ?>" href="/cart" title="Warenkorb"><i class="fa fa-cart-shopping"></i> Warenkorb</a>
+            <a class="<?php echo $site == 'profile' ? 'active' : ''; ?>" href="" title="Benutzer verwalten"><i class="fa fa-user"></i> Benutzer verwalten</a>
+            <a class="<?php echo $site == 'offer' && $site != 'offer/find' ? 'active' : '' ?>" href="/offer" title="Haus vermieten"><i class="fa fa-hand-holding-dollar"></i> Haus vermieten</a>
             <form action="/logout" method="post" style="display: flex; justify-content: center;">
                 <button type="submit" style="font-size: 24px"><i class="fa fa-right-from-bracket"></i> Logout</button>
             </form>
