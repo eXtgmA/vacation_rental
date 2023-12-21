755266191b4c338f08876c24baa73fbe997aa63e
eXtgmA <marvinkuenkler@gmail.com>
2023-12-21 01:45:07 +0100

    close notification on click

--- public/scripts/notification.js
+++ public/scripts/notification.js
@@ -6,21 +6,33 @@
 let notificationPopup = document.createElement("div");
 notificationPopup.id = "notification-popup";
 notificationPopup.classList.add("notification-popup");
-notificationPopup.innerHTML = '<p>ACHTUNG!</p>';
+notificationPopup.innerHTML = '<p>ACHTUNG!</p><button id="close-notification"><i class="fa-solid fa-xmark"></i></button>';
 document.body.appendChild(notificationPopup);
+
+/**
+ * Close the notification popup
+ * @type {HTMLElement}
+ */
+let closeNotificationButton = document.getElementById('close-notification');
+closeNotificationButton.addEventListener('click', function() {
+    notificationPopup.style.display = 'none';
+});
 
 /**
  * Show a notification popup
  * @param message
- * @param time (optional)
+ * @param time (optional) in milliseconds
  */
 function showNotification(message, time = 30000) {
     notificationPopup.querySelector('p').innerHTML = message;
 
     notificationPopup.style.display = 'block';
 
-    // Hide the notification popup after _time_ seconds
     setTimeout(function() {
         notificationPopup.style.display = 'none';
     }, time);
+
+    notificationPopup.addEventListener('click', function() {
+        notificationPopup.style.display = 'none';
+    });
 }
