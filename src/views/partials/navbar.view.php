a7d0f0539b9f2a9e7a5c5053f40f20334eeaf1ec
eXtgmA <marvinkuenkler@gmail.com>
2023-12-22 01:33:57 +0100

    175 - use cart menupoint for checkout

--- src/views/partials/navbar.view.php
+++ src/views/partials/navbar.view.php
@@ -1,6 +1,5 @@
 <?php
 $siteString = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$siteString??'';
 $site = substr((string)$siteString, 1);
 ?>
 <link rel="stylesheet" href="/styles/navbar.css"/>
@@ -9,8 +8,8 @@ $site = substr((string)$siteString, 1);
         <img src="/assets/logo.png" onclick="openLink('/')" alt="logo">
     </div>
     <div class="navlinks">
-        <a class="<?php echo in_array($site, ['dashboard', 'offer/find']) ? 'active' : '' ?>" href="/dashboard" title="Suchen"><i class="fa fa-magnifying-glass"></i></a>
-        <a class="<?php echo $site == 'cart' ? 'active' : ''; ?>" href="/cart" title="Warenkorb"><i class="fa fa-cart-shopping"></i></a>
+        <a class="<?php echo in_array($site, ['', 'dashboard', 'offer/find']) ? 'active' : '' ?>" href="/dashboard" title="Suchen"><i class="fa fa-magnifying-glass"></i></a>
+        <a class="<?php echo in_array($site, ['cart', 'checkout']) ? 'active' : ''; ?>" href="/cart" title="Warenkorb"><i class="fa fa-cart-shopping"></i></a>
         <a class="<?php echo $site == 'profile' ? 'active' : ''; ?>" href="/profile" title="Benutzer verwalten"><i class="fa fa-user"></i></a>
         <a class="<?php echo  (str_contains($site, 'offer') && !str_contains($site, 'find')) ? 'active' : ''; ?>" href="/offer" title="Haus vermieten"><i class="fa fa-hand-holding-dollar"></i></a>
         <form action="/logout" method="post">
@@ -20,8 +19,8 @@ $site = substr((string)$siteString, 1);
     <div class="sandwich-menu">
         <i class="fa fa-bars"></i>
         <div class="sandwich-navlinks">
-            <a class="<?php echo in_array($site, ['dashboard', 'offer/find']) ? 'active' : ''; ?>" href="/dashboard" title="Suchen"><i class="fa fa-magnifying-glass"></i> Suchen</a>
-            <a class="<?php echo $site == 'cart' ? 'active' : ''; ?>" href="/cart" title="Warenkorb"><i class="fa fa-cart-shopping"></i> Warenkorb</a>
+            <a class="<?php echo in_array($site, ['', 'dashboard', 'offer/find']) ? 'active' : ''; ?>" href="/dashboard" title="Suchen"><i class="fa fa-magnifying-glass"></i> Suchen</a>
+            <a class="<?php echo in_array($site, ['cart', 'checkout']) ? 'active' : ''; ?>" href="/cart" title="Warenkorb"><i class="fa fa-cart-shopping"></i> Warenkorb</a>
             <a class="<?php echo $site == 'profile' ? 'active' : ''; ?>" href="" title="Benutzer verwalten"><i class="fa fa-user"></i> Benutzer verwalten</a>
             <a class="<?php echo  (str_contains($site, 'offer') && !str_contains($site, 'find'))  ? 'active' : '' ?>" href="/offer" title="Haus vermieten"><i class="fa fa-hand-holding-dollar"></i> Haus vermieten</a>
             <form action="/logout" method="post" style="display: flex; justify-content: center;">
