c14156d380d17f1fd69a566f79b88a6b0c1e220b
finkeRe <gith-rf@dfr-mail.de>
2023-12-22 19:30:32 +0100

    Remove never used if statement and else block in checkout

--- src/views/checkout.view.php
+++ src/views/checkout.view.php
@@ -19,91 +19,86 @@ if (!(isset($booking) && !empty($bpos) && !empty($houses))) {
     <div class="headline">
         <h1>Kasse</h1>
     </div>
-<?php if (isset($booking) && !empty($bpos) && !empty($houses)) { ?>
-    <?php foreach ($bpos as $key => $p) { ?>
-        <?php $house = $houses[$p->getHouseId()] ?>
-        <div class="" id="cart-entry-grid">
-            <div class="item-headline">
-                <h2><?php echo $house->getName() ?></h2>
-            </div>
-            <div class="item-image">
-                <img src="<?php echo "/images/" . $house->getFrontimage(); ?>" alt="[alt]">
-            </div>
-            <div class="item-location">
-                <div class="informations">
-                    <div class="information">
-                        <span class="information-key">Ort:</span>
-                        <span class="information-value"><?php echo $house->getPostalCode() . ' ' . $house->getCity() ?></span>
-                    </div>
-                    <div class="information">
-                        <span class="information-key">Straße:</span>
-                        <span class="information-value"><?php echo $house->getStreet() . ' ' . $house->getHouseNumber() ?></span>
-                    </div>
+<?php foreach ($bpos as $key => $p) { ?>
+    <?php $house = $houses[$p->getHouseId()] ?>
+    <div class="" id="cart-entry-grid">
+        <div class="item-headline">
+            <h2><?php echo $house->getName() ?></h2>
+        </div>
+        <div class="item-image">
+            <img src="<?php echo "/images/" . $house->getFrontimage(); ?>" alt="[alt]">
+        </div>
+        <div class="item-location">
+            <div class="informations">
+                <div class="information">
+                    <span class="information-key">Ort:</span>
+                    <span class="information-value"><?php echo $house->getPostalCode() . ' ' . $house->getCity() ?></span>
                 </div>
-                <hr/>
-            </div>
-            <div class="item-duration">
-                <div class="informations">
-
-                    <div class="information">
-                        <span class="information-key">Von:</span>
-                        <span class="information-value date-value"><?php echo $p->getDateStart() ?></span>
-                    </div>
-                    <div class="information">
-                        <span class="information-key">Bis:</span>
-                        <span class="information-value date-value"><?php echo $p->getDateEnd() ?></span>
-                    </div>
-                    <div class="information price">
-                        <span class="information-value"><?php echo $house->getPrice(); ?>€</span>
-                    </div>
+                <div class="information">
+                    <span class="information-key">Straße:</span>
+                    <span class="information-value"><?php echo $house->getStreet() . ' ' . $house->getHouseNumber() ?></span>
                 </div>
-                <hr/>
             </div>
-            <?php $pdl = json_decode($p->getPriceDetailList(), true) ?>
-            <?php $optionSum = 0; ?>
-            <div class="item-options <?php echo !empty($pdl) ? '' : 'hide' ?>">
-                <?php if (!empty($pdl)) { ?>
-                    <h3>Optionen</h3>
-                    <?php foreach ($pdl['options'] as $name => $price) { //@phpstan-ignore-line ?>
-                        <div class="option">
-                            <span class="option-key"><?php echo $name //@phpstan-ignore-line ?></span>
-                            <span class="option-value"><?php echo $price //@phpstan-ignore-line ?>€</span>
+            <hr/>
+        </div>
+        <div class="item-duration">
+            <div class="informations">
 
-                        </div>
-                        <?php $optionSum += $price ?>
-                    <?php } ?>
-                    <hr/>
-                    <div class="option-sum option">
-                        <span class="option-key">Summe der Optionen</span>
-                        <span class="option-value"><?php echo $optionSum ?>€</span>
+                <div class="information">
+                    <span class="information-key">Von:</span>
+                    <span class="information-value date-value"><?php echo $p->getDateStart() ?></span>
+                </div>
+                <div class="information">
+                    <span class="information-key">Bis:</span>
+                    <span class="information-value date-value"><?php echo $p->getDateEnd() ?></span>
+                </div>
+                <div class="information price">
+                    <span class="information-value"><?php echo $house->getPrice(); ?>€</span>
+                </div>
+            </div>
+            <hr/>
+        </div>
+        <?php $pdl = json_decode($p->getPriceDetailList(), true) ?>
+        <?php $optionSum = 0; ?>
+        <div class="item-options <?php echo !empty($pdl) ? '' : 'hide' ?>">
+            <?php if (!empty($pdl)) { ?>
+                <h3>Optionen</h3>
+                <?php foreach ($pdl['options'] as $name => $price) { //@phpstan-ignore-line ?>
+                    <div class="option">
+                        <span class="option-key"><?php echo $name //@phpstan-ignore-line ?></span>
+                        <span class="option-value"><?php echo $price //@phpstan-ignore-line ?>€</span>
+
                     </div>
-                    <hr/>
+                    <?php $optionSum += $price ?>
                 <?php } ?>
-            </div>
-            <div class="item-price">
-                <div class="price">
-                    <span class="price-label">Preis:</span>
-                    <span class="price-value"><?php echo $pdl["price"] ?? "Price not calculated!" //@phpstan-ignore-line ?>€</span><!-- todo : get price from list (and delete stan-ignore) -->
+                <hr/>
+                <div class="option-sum option">
+                    <span class="option-key">Summe der Optionen</span>
+                    <span class="option-value"><?php echo $optionSum ?>€</span>
                 </div>
+                <hr/>
+            <?php } ?>
+        </div>
+        <div class="item-price">
+            <div class="price">
+                <span class="price-label">Preis:</span>
+                <span class="price-value"><?php echo $pdl["price"] ?? "Price not calculated!" //@phpstan-ignore-line ?>€</span><!-- todo : get price from list (and delete stan-ignore) -->
             </div>
         </div>
+    </div>
+<?php } ?>
 
-    <?php } ?>
-    <div class="price-footer">
-        <span class="price-label">Gesamtpreis:</span>
-        <span class="price-value">XXXX,XX€</span><!-- todo : calculate value with JS (sum of all prices) -->
-    </div>
-    <div class="pre-footer">
-        <button class="btn-secondary" type="button" onclick="openLink('/cart')">zurück zum Warenkorb</button>
-        <form action='<?php echo "/checkout/booking/".$booking->getId(); ?>' method='post'>
-            <button class="btn-primary" type="submit">Bezahlen</button>
-        </form>
-    </div>
-<?php } else {
-    // if no booking exists
-    echo "<h3>Hier gibt es nichts zu bezahlen. Suche jetzt nach dem Ferienhaus deiner Träume! => <a href='/dashboard'>Zur Suche</a></h3>"; // todo : make me pretty (Marvin)
-}
-?>
+<div class="price-footer">
+    <span class="price-label">Gesamtpreis:</span>
+    <span class="price-value">XXXX,XX€</span><!-- todo : calculate value with JS (sum of all prices) -->
+</div>
+<div class="pre-footer">
+    <button class="btn-secondary" type="button" onclick="openLink('/cart')">zurück zum Warenkorb</button>
+    <form action='<?php echo "/checkout/booking/".$booking->getId(); ?>' method='post'>
+        <button class="btn-primary" type="submit">Bezahlen</button>
+    </form>
+</div>
+
 <?php
 $footer = __DIR__ . "/partials/footer.view.php";
 include_once($footer)
