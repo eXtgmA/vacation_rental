080ebbe308b020963f15a02be45323f93b77b115
finkeRe <gith-rf@dfr-mail.de>
2023-12-29 17:43:03 +0100

    183 - add real suggested houses dashboard - randomly choose 3 houses from db - use default houses as fallback - add them to dashboard view

--- src/controller/DashboardController.php
+++ src/controller/DashboardController.php
@@ -1,6 +1,8 @@
 <?php
 namespace src\controller;
 
+use src\models\House;
+
 class DashboardController extends BaseController
 {
     public function __construct()
@@ -16,6 +18,32 @@ class DashboardController extends BaseController
         }
         // todo : reset filter data
 
-        new ViewController('dashboard');
+        // pick 3 random offers to display on the dashboard
+        /** @var array<string> $suggestedHouses */
+        $suggestedHouses = [];
+        $query = "SELECT * FROM houses WHERE is_disabled=0 ORDER BY RAND() LIMIT 3;";
+        $result = $this->connection()->query($query);
+        if ($result instanceof \mysqli_result) {
+            while ($house = $result->fetch_object('\src\models\House')) {
+                /** @var House $house */
+                $tmp['name'] = $house->getName();
+                $tmp['city'] = $house->getCity();
+                $tmp['price'] = $house->getPrice();
+                $tmp['image'] = "/images/".$house->getFrontImage();
+                $suggestedHouses[] = $tmp;
+            }
+        }
+
+        // if less than 3 houses in database, fill demo house array up with default houses
+        $defaultHousesData = [
+            ["name"=>"Haus auf grüner Wiese", "city"=>"Berlin", "price"=>"750", "image"=>"/assets/haus1.jpg"],
+            ["name"=>"Wohnung am Hang", "city"=>"München", "price"=>"100", "image"=>"/assets/haus2.jpg"],
+            ["name"=>"Modernes Holzhaus", "city"=>"Hamburg", "price"=>"50", "image"=>"/assets/haus3.jpg"]
+        ];
+        for ($i=0; count($suggestedHouses) < 3; $i++) {
+            $suggestedHouses[] = $defaultHousesData[$i];
+        }
+
+        new ViewController('dashboard', $suggestedHouses);
     }
 }
