e04bbc22dcf335755f83af223a5a6b4fee0722c3
finkeRe <gith-rf@dfr-mail.de>
2023-12-30 13:37:22 +0100

    199 - Add missing navbar item highlighting

--- src/views/partials/navbar-blank.view.php
+++ src/views/partials/navbar-blank.view.php
@@ -9,7 +9,7 @@ $site = substr((string)$siteString, 1);
     </div>
     <div class="navlinks">
         <a class="<?php echo in_array($site, ['', 'dashboard'])? 'active':'';?>" href="/dashboard" title="Startseite"><i class="fa-solid fa-house"></i></a>
-        <a class="<?php echo $site=='offer/find'? 'active' : '' ?>" href="/offer/find" title="Suchen"><i class="fa fa-magnifying-glass"></i></a>
+        <a class="<?php echo ($site=='offer/find' || str_starts_with($site, 'offer/detail') )? 'active' : '' ?>" href="/offer/find" title="Suchen"><i class="fa fa-magnifying-glass"></i></a>
         <a class="<?php echo $site=='register'? 'active':'';?>" href="/register" title="Registrieren"><i class="fa fa-user-plus"></i></a>
         <a class="<?php echo $site=='login'? 'active':'';?>" href="/login" title="Anmelden"><i class="fa fa-key"></i></a>
     </div>
@@ -17,7 +17,7 @@ $site = substr((string)$siteString, 1);
         <i class="fa fa-bars"></i>
         <div class="hamburger-navlinks">
             <a class="<?php echo in_array($site, ['', 'dashboard'])? 'active':'';?>" href="/dashboard"><i class="fa-solid fa-house"></i> Startseite</a>
-            <a class="<?php echo $site=='offer/find'? 'active':'';?>" href="/offer/find"><i class="fa fa-magnifying-glass"></i> Suche</a>
+            <a class="<?php echo ($site=='offer/find' || str_starts_with($site, 'offer/detail') )? 'active':'';?>" href="/offer/find"><i class="fa fa-magnifying-glass"></i> Suche</a>
             <a class="<?php echo $site=='register'? 'active':'';?>" href="/register"><i class="fa fa-user-plus"></i> Registrieren</a>
             <a class="<?php echo $site=='login'? 'active':'';?>" href="/login"><i class="fa fa-key"></i> Login</a>
         </div>
