61d535d32655cf4e9ea5bf5f54307e97f31a58a2
bbuermann-it <buermann.bjoern@gmail.com>
2024-01-02 18:01:13 +0100

    :bug: added strict redirecting when user is  not logged in.

--- src/controller/CartController.php
+++ src/controller/CartController.php
@@ -14,6 +14,7 @@ class CartController extends BaseController
 
     public function getCart(): void
     {
+        $this->redirectIfNotLoggedIn();
         try {
             // get the one and only booking where "is_confirmed" equals false belonging to current user
             $query = "select * from bookings where user_id = {$_SESSION['user']} and is_confirmed = 0 limit 1";
